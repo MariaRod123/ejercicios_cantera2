6069e040dae9901b9f78110e567047ab7f77b458
María <72228855+MariaRod123@users.noreply.github.com>
2022-11-10 21:28:41 -0300

    Create Ejercicio1.java

---
+++ Ejercicio1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        String nombre;
+        String apellido;
+
+
+        System.out.println("Ingrese su nombre");
+        Scanner input = new Scanner (System.in);
+        nombre=input.next();
+
+        System.out.println("Ingrese su apellido");
+        apellido= input.next();
+
+        System.out.println("Su nombre es:" + nombre + "\nSu apellido es: " + apellido);
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
