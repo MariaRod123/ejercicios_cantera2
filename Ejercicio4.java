f26e898957b47bd31392df11af083aa01ffd2655
María <72228855+MariaRod123@users.noreply.github.com>
2022-11-16 22:11:05 -0300

    Update Ejercicio4.java

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -6,12 +6,11 @@ public class Ejercicio4 {
         String ciudad, pais;
 
         System.out.println("Ingresar una ciudad");
-        Scanner ciud = new Scanner (System.in);
-        ciudad=ciud.next();
+        Scanner input = new Scanner (System.in);
+        ciudad=input.nextLine();
 
         System.out.println("Ingresar un pais");
-        Scanner paisito = new Scanner(System.in);
-        pais= paisito.next();
+        pais= input.nextLine();
 
         System.out.println("La ciudad " + ciudad + " es la capital de " + pais);
     }
