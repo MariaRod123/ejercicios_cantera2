667cfec31a42f937a97a23a02fd8eca3726f4368
María <72228855+MariaRod123@users.noreply.github.com>
2022-11-16 22:19:05 -0300

    Update Ejercicio5.java

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Ejercicio5 {
     public static void main(String[] args) {
-        String nombre_mascota, nombre_duenio, raza;
+        String nombre_mascota, nombre_duenio, tipo_mascota;
         int edad_mascota;
 
         System.out.println("Ingrese el nombre de su mascota");
@@ -13,15 +13,15 @@ public class Ejercicio5 {
         Scanner anios = new Scanner(System.in);
         edad_mascota= anios.nextInt();
 
-        System.out.println("Ingrese la raza de la mascota");
-        Scanner ra_za = new Scanner (System.in);
-        raza = ra_za.nextLine();
+        System.out.println("Ingrese el tipo de mascota");
+        Scanner tipo = new Scanner (System.in);
+        tipo_mascota= tipo.nextLine();
 
         System.out.println("Ingrese el nombre completo del dueño de la mascota");
         Scanner duenio = new Scanner (System.in);
         nombre_duenio = duenio.nextLine();
 
-        System.out.println(nombre_mascota + " es un/a " + raza + " el/la cual tiene " + edad_mascota + " años" + " y " + nombre_duenio + " es actualmente su dueño/a");
+        System.out.println(nombre_mascota + " es un/a " + tipo_mascota + " el/la cual tiene " + edad_mascota + " años" + " y " + nombre_duenio + " es actualmente su dueño/a");
     }
 }
 
