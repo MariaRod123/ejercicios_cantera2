341116a33d268345504318abbea94a425b9f1c75
María <72228855+MariaRod123@users.noreply.github.com>
2022-11-20 22:11:49 -0300

    Update EjercicioArreglos2.java

--- EjercicioArreglos2.java
+++ EjercicioArreglos2.java
@@ -7,30 +7,30 @@ public class EjercicioArreglos2 {
         int[] vector = new int[20];
         ArrayList<Integer> pares = new ArrayList<Integer>();
         ArrayList<Integer> impares = new ArrayList<Integer>();
-        int min = 1, max = 100; /*maximo y minimo del rango de aleatoriedad solicitada*/
+        int min = 1, max = 100; //maximo y minimo del rango de aleatoriedad solicitada
 
         for (int i = 0; i < vector.length; i++) {
-            vector[i] = (int) Math.floor(Math.random() * (max - min + 1) + min); /*vector que guarda  números aleatorios*/
+            vector[i] = (int) Math.floor(Math.random() * (max - min + 1) + min); //vector que guarda  números aleatorios
 
         }
 
         for (int i = 0; i < vector.length; i++) {
 
             if (vector[i] % 2 == 0) {
-                pares.add(vector[i]); /* agrega en un array todos los números pares que encontró en el conjunto*/
+                pares.add(vector[i]); // agrega en un array todos los números pares que encontró en el conjunto
             }
 
         }
-        System.out.println(pares); /* muestra el nuevo array generado por los numeros pares encontrados */
+        System.out.println(pares); //muestra el nuevo array generado por los numeros pares encontrados 
 
         for (int i = 0; i < vector.length; i++) {
 
             if (vector[i] % 2 != 0) {
-                impares.add(vector[i]); /* agrega en un array todos los números impares que tiene el conjunto*/
+                impares.add(vector[i]); // agrega en un array todos los números impares que tiene el conjunto
 
             }
         }
-        System.out.println(impares); /* muestra el nuevo array generado por los numeros impares encontrados */
+        System.out.println(impares); //muestra el nuevo array generado por los numeros impares encontrados */
     }
 
 }
